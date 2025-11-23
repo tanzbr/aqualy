25e062c014b4d89e71be1daf223845b0facbb58a
Tanz <cauamelo2345@gmail.com>
2025-11-23 01:55:39 -0300

    fix do fix

--- aqualy-backend/src/main/java/br/unitins/topicos1/service/LeituraPiezoServiceImpl.java
+++ aqualy-backend/src/main/java/br/unitins/topicos1/service/LeituraPiezoServiceImpl.java
@@ -10,7 +10,9 @@ import br.unitins.topicos1.dto.LeituraPiezoDTO;
 import br.unitins.topicos1.dto.LeituraPiezoResponseDTO;
 import br.unitins.topicos1.model.LeituraPiezo;
 import br.unitins.topicos1.repository.LeituraPiezoRepository;
+import io.quarkus.narayana.jta.QuarkusTransaction;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.control.ActivateRequestContext;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -35,18 +37,21 @@ public class LeituraPiezoServiceImpl implements LeituraPiezoService {
     }
 
     @Override
+    @ActivateRequestContext
     public DadosGraficoDTO obterDadosUltimos60Segundos(String sensorId) {
-        List<LeituraPiezo> leituras = leituraPiezoRepository.findUltimosSegundos(sensorId, 60);
-        
-        List<LocalDateTime> timestamps = leituras.stream()
-                .map(LeituraPiezo::getDataHora)
-                .collect(Collectors.toList());
-        
-        List<BigDecimal> valores = leituras.stream()
-                .map(LeituraPiezo::getValor)
-                .collect(Collectors.toList());
-        
-        return DadosGraficoDTO.criar(sensorId, timestamps, valores);
+        return QuarkusTransaction.requiringNew().call(() -> {
+            List<LeituraPiezo> leituras = leituraPiezoRepository.findUltimosSegundos(sensorId, 60);
+            
+            List<LocalDateTime> timestamps = leituras.stream()
+                    .map(LeituraPiezo::getDataHora)
+                    .collect(Collectors.toList());
+            
+            List<BigDecimal> valores = leituras.stream()
+                    .map(LeituraPiezo::getValor)
+                    .collect(Collectors.toList());
+            
+            return DadosGraficoDTO.criar(sensorId, timestamps, valores);
+        });
     }
 }
 
